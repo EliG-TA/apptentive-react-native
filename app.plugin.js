ed191a28a842316ca46d5921240ceeb18508ff1b
NightScript <18664762+NightScript370@users.noreply.github.com>
2024-02-07 00:24:45 -0500

    Also add value to the styles config

--- app.plugin.js
+++ app.plugin.js
@@ -3,11 +3,19 @@
 const expoCP = require('@expo/config-plugins')
 
 /** @type {expoCP.ConfigPlugin} */
-function expoModifyPlugin (config) {
-    return expoCP.withAndroidColors(config, config => {
-        config.modResults = expoCP.AndroidConfig.Colors.assignColorValue(config.modResults, { name: "colorSurface", "value": "#000000" })
-        return config
-    })
+function expoModifyPlugin (baseConfig) {
+    const colorExpoConfig = expoCP
+        .withAndroidColors(baseConfig, config => {
+            config.modResults = expoCP.AndroidConfig.Colors.assignColorValue(config.modResults, { name: "colorSurface", "value": "#000000" })
+            return config
+        })
+    const expoThemeConfig = expoCP
+        .withAndroidStyles(colorExpoConfig, config => {
+            config.modResults = expoCP.AndroidConfig.Styles.assignStylesValue(config.modResults, { name: "colorSurface", "value": "@color/colorSurface", add: true, parent: expoCP.AndroidConfig.Styles.getAppThemeLightNoActionBarGroup()})
+            return config
+        })
+
+    return expoThemeConfig;
 }
 
 module.exports = expoModifyPlugin;
